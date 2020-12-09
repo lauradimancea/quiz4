bcdfcb51a7b4c197fe52a11e39fc8b742daf3427
Laura Dimancea <laura_dimancea@smart-x.net>
2020-12-09 19:12:00 +0200

    task 3

--- src/main/java/com/example/quiz4/controller/SumController.java
+++ src/main/java/com/example/quiz4/controller/SumController.java
@@ -2,11 +2,9 @@ package com.example.quiz4.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("sum")
 public class SumController {
 
     @GetMapping(path = "/add/first/{first}/second/{second}")
